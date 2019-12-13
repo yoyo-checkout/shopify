b54f28cd73fe71fffe1524d7d1f3b218cc3ed682
江宗祐 <zongyojiang@Zongyojiang-MNB.local>
2019-12-13 17:32:55 +0800

    fetch data with apollo

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const dotenv = require('dotenv');
 const Koa = require('koa');
 const next = require('next');
 const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
-const { verifyRequest } = require('@shopify/koa-shopify-auth');
+const {verifyRequest} = require('@shopify/koa-shopify-auth');
 const session = require('koa-session');
 
 dotenv.config();
+const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
+const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
+const app = next({dev});
 const handle = app.getRequestHandler();
 
 const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
@@ -24,15 +26,15 @@ app.prepare().then(() => {
     createShopifyAuth({
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET_KEY,
-      scopes: ['read_products'],
+      scopes: ['read_products', 'write_products'],
       afterAuth(ctx) {
-        const { shop, accessToken } = ctx.session;
-        ctx.cookies.set('shopOrigin', shop, { httpOnly: false });
+        ctx.cookies.set('shopOrigin', shop, { httpOnly: false })
         ctx.redirect('/');
       },
     }),
   );
 
+  server.use(graphQLProxy({version: ApiVersion.October19}))
   server.use(verifyRequest());
   server.use(async (ctx) => {
     await handle(ctx.req, ctx.res);
